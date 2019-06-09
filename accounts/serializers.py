9127658b56fe0d4998ab38cd2ed9f430273ebfa7
Monalisa Mello <monacacheada@gmail.com>
2019-06-09 16:57:29 -0300

    Updating installed_apps and user serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,11 +1,27 @@
 from django.contrib.auth.models import User, Group
+from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(
+            validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(min_length=8, style={'input_type': 'password'})
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+                validated_data['username'], 
+                validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'password', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
