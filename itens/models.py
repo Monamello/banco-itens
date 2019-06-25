f4d411013a4da8b3757aac405223a942801820bb
Monamello <monacacheada@gmail.com>
2019-06-24 21:45:59 -0300

    - add novo campo para suporte.Item  - Migrates

--- itens/models.py
+++ itens/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User, Group
 
 class Item(models.Model):
     enunciado = models.TextField(max_length=1200)
-    suporte = models.TextField(max_length=1200)
-    comando = models.TextField(max_length=1200)
+    suporte_texto = models.TextField(max_length=1200, blank=True, null=True)
+    suporte_imagem = models.ImageField(upload_to='imagem_alternativas', blank=True, null=True)
+    comando = models.TextField(max_length=1200, blank=False, null=False)
     dificuldade = models.CharField(max_length=30, choices=DIFICULDADES)
     cursos = models.ManyToManyField('Cursos')
     unidades_curriculares = models.ForeignKey('UnidadeCurricular', related_name='unidades_curriculares', on_delete=models.CASCADE)
