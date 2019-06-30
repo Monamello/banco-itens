7d5369410cb5b4bb6b205976ba1fe2540914c1a1
Monamello <monacacheada@gmail.com>
2019-06-30 01:19:06 -0300

    - add suporte imagem  - setando required false para as imagens  - trocando forms.Form por ModelForm

--- itens/forms.py
+++ itens/forms.py
@@ -1,11 +1,17 @@
 from django import forms
+from django.forms import ModelForm
 from .choices import DIFICULDADES
 from .models import Item, Cursos, UnidadeCurricular, Alternativa
 
 
-class ItemForm(forms.Form):
+class ItemForm(ModelForm):
+    class Meta:
+        model = Item
+        exclude = ()
+
     enunciado = forms.CharField(widget=forms.Textarea)
     suporte = forms.CharField(widget=forms.Textarea)
+    suporte_imagem = forms.ImageField(required=False, label='Suporte Imagem')
     comando = forms.CharField(widget=forms.Textarea)
     dificuldade = forms.ChoiceField(choices=DIFICULDADES, label="Dificuldade", widget=forms.Select())
     cursos = forms.MultipleChoiceField(
@@ -16,7 +22,11 @@ class ItemForm(forms.Form):
         label='Curso', widget=forms.SelectMultiple())
 
 
-class AlternativaForm(forms.Form):
+class AlternativaForm(ModelForm):
+    class Meta:
+        model = Alternativa
+        exclude = ()
+
     texto = forms.CharField(widget=forms.Textarea)
-    imagem = forms.ImageField()
+    imagem = forms.ImageField(required=False)
     correta = forms.BooleanField()
